6d20be84c431eaec31c3bcce434b8b514725c349
Letícia Maria Pequeno Madureira <leticiapequeno30@gmail.com>
2022-01-04 20:33:47 -0300

    Update Divinity.jl

--- src/Divinity.jl
+++ src/Divinity.jl
@@ -1,5 +1,20 @@
 module Divinity
 
-greet() = print("Hello World!")
+function dat2csv(dat_path::AbstractString, csv_path::AbstractString)
+    open(csv_path, write=true) do io
+        for line in eachline(dat_path)
+            join(io, split(line), ',')
+            println(io)
+        end
+    end
+    return csv_path
+end
+
+function dat2csv(dat_path::AbstractString)
+    base, ext = splitext(dat_path)
+    ext == ".dat" ||
+        throw(ArgumentError("file name doesn't end with `.dat`"))
+    return dat2csv(dat_path, "$base.csv")
+end
 
 end # module
